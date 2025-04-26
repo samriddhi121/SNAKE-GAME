f9d7635634c55aa6508a6c013acd991ce4049306
samriddhi121 <Samriddhisingh4741@gmail.com>
2025-04-26 23:40:20 +0530

    Add files via upload

---
+++ snake game.py
@@ -0,0 +1,48 @@
+Python 3.11.9 (tags/v3.11.9:de54cf5, Apr  2 2024, 10:12:12) [MSC v.1938 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+===================== RESTART: C:\Users\new tech\task2.0.py ====================
+[[0 0 0]
+ [0 0 0]
+ [0 0 0]]
+Board after 1 move
+[[1 0 0]
+ [0 0 0]
+ [0 0 0]]
+Board after 2 move
+[[1 2 0]
+ [0 0 0]
+ [0 0 0]]
+Board after 3 move
+[[1 2 1]
+ [0 0 0]
+ [0 0 0]]
+Board after 4 move
+[[1 2 1]
+ [0 0 2]
+ [0 0 0]]
+Board after 5 move
+[[1 2 1]
+ [0 0 2]
+ [0 0 1]]
+Board after 6 move
+[[1 2 1]
+ [0 2 2]
+ [0 0 1]]
+Board after 7 move
+[[1 2 1]
+ [0 2 2]
+ [0 1 1]]
+Board after 8 move
+[[1 2 1]
+ [0 2 2]
+ [2 1 1]]
+Board after 9 move
+[[1 2 1]
+ [1 2 2]
+ [2 1 1]]
+Winner is: -1
+>>> 
+================== RESTART: C:\Users\new tech\digitalclock.py ==================
+>>> 
+====================== RESTART: C:\Users\new tech\SNAKE.PY =====================
